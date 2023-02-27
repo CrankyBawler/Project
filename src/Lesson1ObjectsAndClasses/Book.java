80dbcd06a5dc4cbeb8810bb297a13bd4253aaae5
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-27 21:22:19 +0300

    1

--- src/Lesson1ObjectsAndClasses/Book.java
+++ src/Lesson1ObjectsAndClasses/Book.java
@@ -2,10 +2,10 @@ package Lesson1ObjectsAndClasses;
 
 public class Book {
     private String bookName;
-    private String authorName;
+    private Author authorName;
     private int publishingYear;
 
-    public Book(String bookName, String authorName, int publishingYear) {
+    public Book(String bookName, Author authorName, int publishingYear) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.publishingYear = publishingYear;
@@ -15,7 +15,7 @@ public class Book {
         return this.bookName;
     }
 
-    public String getAuthorName() {
+    public Author getAuthorName() {
         return this.authorName;
     }
         public int getPublishingYear () {
--- src/Lesson1ObjectsAndClasses/Main.java
+++ src/Lesson1ObjectsAndClasses/Main.java
@@ -3,24 +3,19 @@ package Lesson1ObjectsAndClasses;
 public class Main {
     public static void main(String[] args) {
         Author tolstoy = new Author("Лев", "Толстой");
-        String tolstoyName = tolstoy.getName();
-        String tolstoyFamilia = tolstoy.getFamiliaName();
-        String tolsoyFullName = tolstoyName + " " + tolstoyFamilia;
         Author dostoevski = new Author("Фёдор", "Достаевский");
-        String dostaevskiyName = dostoevski.getName();
-        String dostaevskiyFamilia = dostoevski.getFamiliaName();
-        String dostaevskiyFullName = dostaevskiyName + " " + dostaevskiyFamilia;
         Author tyrgenev = new Author("Иван", "Тургенев");
-        String tyrgenevName = tyrgenev.getName();
-        String tyrgenevFamilia = tyrgenev.getFamiliaName();
-        String tyrgenevFullName = tyrgenevName + " " + tyrgenevFamilia;
-        Book warAndPeace = new Book("Война и мир", tolsoyFullName, 1867);
-        Book krimeAndPunishment = new Book("Преступление и наказание", dostaevskiyFullName, 1866);
-        Book fathersAndSons = new Book("Отцы и дети", tyrgenevFullName, 1862);
-        System.out.println(warAndPeace.getBookName() + ", " + warAndPeace.getAuthorName() + ", " + warAndPeace.getPublishingYear());
-        System.out.println(fathersAndSons.getBookName() + ", " + fathersAndSons.getAuthorName() + ", " + fathersAndSons.getPublishingYear());
-        System.out.println(krimeAndPunishment.getBookName() + ", " + krimeAndPunishment.getAuthorName() + ", " + krimeAndPunishment.getPublishingYear());
+        Book warAndPeace = new Book("Война и мир", tolstoy, 1867);
+        Book krimeAndPunishment = new Book("Преступление и наказание", dostoevski, 1866);
+        Book fathersAndSons = new Book("Отцы и дети", tyrgenev, 1862);
+        System.out.println(warAndPeace.getBookName() + ", " + warAndPeace.getAuthorName().getName() + " " + warAndPeace.
+                getAuthorName().getFamiliaName() + ", " + warAndPeace.getPublishingYear());
+        System.out.println(fathersAndSons.getBookName() + ", " + fathersAndSons.getAuthorName().getName() + " " +
+                        fathersAndSons.getAuthorName().getFamiliaName() + ", " + fathersAndSons.getPublishingYear());
+        System.out.println(krimeAndPunishment.getBookName() + ", " + krimeAndPunishment.getAuthorName().getName() + " "
+                + krimeAndPunishment.getAuthorName().getFamiliaName() + ", " + krimeAndPunishment.getPublishingYear());
         krimeAndPunishment.setPublishingYear(1998);
-        System.out.println(krimeAndPunishment.getBookName() + ", " + krimeAndPunishment.getAuthorName() + ", " + krimeAndPunishment.getPublishingYear());
+        System.out.println(krimeAndPunishment.getBookName() + ", " + krimeAndPunishment.getAuthorName().getName() + " "
+                + krimeAndPunishment.getAuthorName().getFamiliaName() + ", " + krimeAndPunishment.getPublishingYear());
     }
 }
